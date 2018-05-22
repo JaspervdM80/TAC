181f1eec8d832b0f7e04bb5847f7e3ddb623d7af
Jaspernl <jasperm@macaw>
2018-05-22 22:48:50 +0200

    Module 7

---
+++ events.tac.local/Business/RelatedEventsProvider.cs
@@ -0,0 +1,35 @@
+﻿using events.tac.local.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TAC.Sitecore.Abstractions.Interfaces;
+using TAC.Sitecore.Abstractions.SitecoreImplementation;
+
+namespace events.tac.local.Business
+{
+    public class RelatedEventsProvider
+    {
+        private IRenderingContext _context;
+
+        public RelatedEventsProvider() : this(SitecoreRenderingContext.Create()) { }
+
+        public RelatedEventsProvider(IRenderingContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<NavigationItem> GetRelatedEvents()
+        {
+            var items = _context.ContextItem.GetMultilistFieldItems("relatedEvents").ToList();
+
+            return items.Select(i =>
+                                new NavigationItem(
+                                    i.DisplayName,
+                                    i.Url,
+                                    true
+                                    ));
+        }
+
+    }
+}
